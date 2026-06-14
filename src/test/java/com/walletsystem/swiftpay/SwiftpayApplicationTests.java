c02391b538964b5f158777b44d9d39d2db78c064
CrowningCoder <shivanshsrivastava03012k@gmail.com>
2026-06-14 13:50:15 +0530

    Complete authentication and authorization module

---
+++ src/test/java/com/walletsystem/swiftpay/SwiftpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletsystem.swiftpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
